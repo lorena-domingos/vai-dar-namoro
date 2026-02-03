a6e14586f2b93eba409410d89d704632e31b0cd9
Lorena Mendes <212278877+lorena-domingos@users.noreply.github.com>
2026-02-02 23:37:37 -0300

    feat: projeto finalizado, adicionado música, imagem e estilização

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Será que vai dar namoro?
+
+Um projeto bobo, só pra brincar com JS e com os amigos.
+
+Música e imagem utilizada que não é de minha autoria:
+- Hometown Cha-Cha-Cha OST - Romantic Sunday (Car the Garden)
+- Beijo Explosivo (Assistam!)
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const NovoTexto = document.querySelector("#TextoAparece");
 const ImagemLinda = document.querySelector(".ImagemDesaparece");
+const SomLindo = new Audio("Hometown Cha-Cha-Cha OST - Romantic Sunday (Car the Garden).mp3")
 
 function Btn() {
     ImagemLinda.classList.remove("ImagemDesaparece");
-    NovoTexto.innerHTML = "Vai dar namoro SIM!";
+    NovoTexto.innerHTML = "Vai dar namoro <strong>SIM!</strong>";
+    SomLindo.loop = true;
+    SomLindo.play();
 };
